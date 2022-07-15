53bdf9b6270e3b808fe63e738248f2dcd27b2164
Ranguiner Daniel <103294791+RanguinerDaniel@users.noreply.github.com>
2022-07-15 02:32:34 -0400

    Update Main.java

--- src/com/poo/java/Main.java
+++ src/com/poo/java/Main.java
@@ -5,18 +5,15 @@ import java.util.Scanner;
 
 import com.entities.java.Triangle;
 
-
 public class Main {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
-		double p; 
-		
+
 		Triangle x, y;
 		x = new Triangle();
 		y = new Triangle();
-		
+
 		System.out.println("Digite as medidas do triângulo X: ");
 		x.a = sc.nextFloat();
 		x.b = sc.nextFloat();
@@ -27,14 +24,12 @@ public class Main {
 		y.b = sc.nextFloat();
 		y.c = sc.nextFloat();
 
-		p = (x.a + x.b + x.c) / 2;
-		double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
+		double areaX = x.area();
 
-		p = (y.a + y.b + y.c) / 2;
-		double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+		double areaY = y.area();
 
-		System.out.printf("Área do triângulo X: %.4f", areaX);
-		System.out.printf("Área do triângulo Y: %.4f", areaY);
+		System.out.printf("Área do triângulo X: %.4f%n", areaX);
+		System.out.printf("Área do triângulo Y: %.4f%n", areaY);
 
 		if (areaX > areaY) {
 			System.out.println("o triângulo X possui a maior área.");
